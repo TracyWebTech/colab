696c749a63631d0f1e4908004bc354d12dbaac50
Luan <luanpab@gmail.com>
2013-12-09 16:15:13 -0200

    Adding rss to user profile

--- src/rss/feeds.py
+++ src/rss/feeds.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from django.core.urlresolvers import NoReverseMatch
 from django.contrib.syndication.views import Feed
 from django.utils.translation import ugettext as _
 
 from haystack.query import SearchQuerySet
 
-from super_archives.models import Thread
+from super_archives.models import Thread, Message
 
 
 class LatestThreadsFeeds(Feed):
@@ -71,3 +72,62 @@ class LatestColabFeeds(Feed):
 
     def item_link(self, item):
         return item.url
+
+
+class LatestUserThreadsFeeds(Feed):
+    title = _(u'Latest User Discussions')
+    link = '/rss/threads/latest/user/'
+
+    def __call__(self, request, *args, **kwargs):
+        self.username = kwargs.get('username')
+
+        return super(LatestUserThreadsFeeds, self).__call__(
+            request, *args, **kwargs
+        )
+
+    def items(self):
+        return Message.objects.filter(
+            from_address__user__username=self.username
+        )[:20]
+
+    def item_link(self, item):
+        try:
+            return item.thread.get_absolute_url()
+        except NoReverseMatch:
+            return ''
+
+    def item_title(self, item):
+        title = '[' + item.mailinglist.name + '] '
+        title += item.subject_clean
+        return title
+
+    def item_description(self, item):
+        return item.body
+
+
+class LatestUserContributionsFeeds(Feed):
+    title = _(u'Latest User Contributions')
+    link = '/rss/colab/latest/user/'
+
+    def __call__(self, request, *args, **kwargs):
+        self.username = kwargs.get('username')
+
+        return super(LatestUserContributionsFeeds, self).__call__(
+            request, *args, **kwargs
+        )
+
+    def items(self):
+        return SearchQuerySet().filter(
+            collaborators__contains=self.username,
+        )[:20]
+
+    def item_link(self, item):
+        return item.url
+
+    def item_title(self, item):
+        title = '[' + item.title + '] '
+        return title
+
+    def item_description(self, item):
+        descr = item.description
+        return descr if len(descr) <= 800 else descr[:797] + '...'
--- src/rss/urls.py
+++ src/rss/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import patterns, url
-import feeds 
+import feeds
 
 urlpatterns = patterns('',
     url(r'threads/latest/$', feeds.LatestThreadsFeeds(), name='rss_latest_threads'),
     url(r'colab/latest/$', feeds.LatestColabFeeds(), name='rss_latest_colab'),
     url(r'threads/hottest/$', feeds.HottestThreadsFeeds(), name='rss_hottest_threads'),
+    url(
+        r'threads/latest/(?P<username>[\w+.-]+)/$',
+        feeds.LatestUserThreadsFeeds(),
+        name='rss_user_latest_threads'
+    ),
+    url(
+        r'colab/latest/(?P<username>[\w+.-]+)/$',
+        feeds.LatestUserContributionsFeeds(),
+        name='rss_user_latest_contributions'
+    ),
 )
 
