bbfb4adcc1d9099a975f4705b0144b30dd1b13bc
Luan <luanpab@gmail.com>
2013-12-13 09:10:30 -0200

    Adding home chart with pygal

---
+++ src/home/utils.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from pygal.style import Style
+
+
+CustomLightStyle = Style(
+    background = 'transparent',
+    plot_background = 'transparent',
+    foreground='rgba(0, 0, 0, 0.7)',
+    foreground_light='rgba(0, 0, 0, 0.9)',
+    foreground_dark='rgba(0, 0, 0, 0.5)',
+    colors=('#242424', '#9f6767', '#92ac68',
+            '#d0d293', '#9aacc3', '#bb77a4',
+            '#77bbb5', '#777777')
+)
--- src/home/views.py
+++ src/home/views.py
@@ -1,14 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import pygal
 
 from collections import OrderedDict
 
 from django.core.cache import cache
 from django.shortcuts import render
+from django.utils.translation import ugettext as _
 
 from search.utils import trans
 from haystack.query import SearchQuerySet
 
 from proxy.models import WikiCollabCount, TicketCollabCount
 from super_archives.models import Thread
+from .utils import CustomLightStyle
 
 
 def index(request):
@@ -18,8 +23,8 @@ def index(request):
     latest_threads = Thread.objects.all()[:6]
     hottest_threads = Thread.highest_score.from_haystack()[:6]
 
-    count_types = cache.get('home_chart')
-    if count_types is None:
+    pie_chart_dict = cache.get('home_chart')
+    if pie_chart_dict is None:
         count_types = OrderedDict()
         for type in ['thread', 'changeset', 'attachment']:
             count_types[trans(type)] = SearchQuerySet().filter(
@@ -33,12 +38,29 @@ def index(request):
         count_types[trans('wiki')] = sum([
             wiki.count for wiki in WikiCollabCount.objects.all()
         ])
-        cache.set('home_chart', count_types)
+
+        PIE_WIDTH = 500
+        PIE_HEIGHT = 350
+        pie_chart = pygal.Pie(
+            style=CustomLightStyle, width=PIE_WIDTH, height=PIE_HEIGHT,
+            no_data_text=_(u'No data found'), value_font_size=15
+        )
+        for count in tuple(count_types.items()):
+            pie_chart.add(count[0], count[1])
+
+        pie_chart_dict = dict(
+            render=pie_chart.make_instance(
+                overrides={'disable_xml_declaration': True}
+            ).render(is_unicode=True),
+            width=PIE_WIDTH,
+            height=PIE_HEIGHT,
+        )
+        cache.set('home_chart', pie_chart_dict)
 
     context = {
         'hottest_threads': hottest_threads[:6],
         'latest_threads': latest_threads,
-        'type_count': count_types,
+        'pie_chart': pie_chart_dict,
         'latest_results': SearchQuerySet().all().order_by(
             '-modified', '-created'
         )[:6],
